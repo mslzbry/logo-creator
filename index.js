363d4441e2b60c69f3e07e668dc3fd7dd1b753c2
Michael Salisbury <m.slzbry@gmail.com>
2023-06-11 21:14:43 -0700

    Add shape objects and svg render

--- index.js
+++ index.js
@@ -2,19 +2,162 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 
+const FILENAME = 'logo.svg' // name of the generated logo file
+
+// list of all possible color keywords supported by svg
+// reference: https://www.w3.org/TR/SVG11/types.html#ColorKeywords
 const COLORS = [
-  'red',
-  'orange',
-  'yellow',
-  'green',
-  'blue',
-  'purple',
+  'aliceblue',
+  'antiquewhite',
+  'aqua',
+  'aquamarine',
+  'azure',
+  'beige',
+  'bisque',
   'black',
+  'blanchedalmond',
+  'blue',
+  'blueviolet',
   'brown',
-  'white',
+  'burlywood',
+  'cadetblue',
+  'chartreuse',
+  'chocolate',
+  'coral',
+  'cornflowerblue',
+  'cornsilk',
+  'crimson',
+  'cyan',
+  'darkblue',
+  'darkcyan',
+  'darkgoldenrod',
+  'darkgray',
+  'darkgreen',
+  'darkgrey',
+  'darkkhaki',
+  'darkmagenta',
+  'darkolivegreen',
+  'darkorange',
+  'darkorchid',
+  'darkred',
+  'darksalmon',
+  'darkseagreen',
+  'darkslateblue',
+  'darkslategray',
+  'darkslategrey',
+  'darkturquoise',
+  'darkviolet',
+  'deeppink',
+  'deepskyblue',
+  'dimgray',
+  'dimgrey',
+  'dodgerblue',
+  'firebrick',
+  'floralwhite',
+  'forestgreen',
+  'fuchsia',
+  'gainsboro',
+  'ghostwhite',
+  'gold',
+  'goldenrod',
   'gray',
-  'pink'
+  'green',
+  'greenyellow',
+  'grey',
+  'honeydew',
+  'hotpink',
+  'indianred',
+  'indigo',
+  'ivory',
+  'khaki',
+  'lavender',
+  'lavenderblush',
+  'lawngreen',
+  'lemonchiffon',
+  'lightblue',
+  'lightcoral',
+  'lightcyan',
+  'lightgoldenrodyellow',
+  'lightgray',
+  'lightgreen',
+  'lightgrey',
+  'lightpink',
+  'lightsalmon',
+  'lightseagreen',
+  'lightskyblue',
+  'lightslategray',
+  'lightslategrey',
+  'lightsteelblue',
+  'lightyellow',
+  'lime',
+  'limegreen',
+  'linen',
+  'magenta',
+  'maroon',
+  'mediumaquamarine',
+  'mediumblue',
+  'mediumorchid',
+  'mediumpurple',
+  'mediumseagreen',
+  'mediumslateblue',
+  'mediumspringgreen',
+  'mediumturquoise',
+  'mediumvioletred',
+  'midnightblue',
+  'mintcream',
+  'mistyrose',
+  'moccasin',
+  'navajowhite',
+  'navy',
+  'oldlace',
+  'olive',
+  'olivedrab',
+  'orange',
+  'orangered',
+  'orchid',
+  'palegoldenrod',
+  'palegreen',
+  'paleturquoise',
+  'palevioletred',
+  'papayawhip',
+  'peachpuff',
+  'peru',
+  'pink',
+  'plum',
+  'powderblue',
+  'purple',
+  'red',
+  'rosybrown',
+  'royalblue',
+  'saddlebrown',
+  'salmon',
+  'sandybrown',
+  'seagreen',
+  'seashell',
+  'sienna',
+  'silver',
+  'skyblue',
+  'slateblue',
+  'slategray',
+  'slategrey',
+  'snow',
+  'springgreen',
+  'steelblue',
+  'tan',
+  'teal',
+  'thistle',
+  'tomato',
+  'turquoise',
+  'saddlebrown',
+  'violet',
+  'wheat',
+  'white',
+  'whitesmoke',
+  'yellow',
+  'yellowgreen'
 ]
+
+const { Triangle, Square, Circle } = require('./shapes')
 
 // regex reference: https://www.geeksforgeeks.org/how-to-validate-hexadecimal-color-code-using-regular-expression/#
 const regHex = new RegExp('^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$') // use regex to determine if user input is a hex color
@@ -43,7 +186,7 @@ const questions = [
   },
   {
     type: 'input',
-    name: 'color',
+    name: 'textColor',
     message: 'Enter a color keyword (OR a hexadecimal number):',
     validate: validateColor
   },
@@ -55,25 +198,41 @@ const questions = [
   },
   {
     type: 'input',
-    message:
-      'Enter a color keyword for the shape (Enter color keyword OR a hexadecimal number)',
-    name: 'shapeBackgroundColor',
+    message: 'Enter a color keyword for the shape (OR a hexadecimal number)',
+    name: 'bgColor',
     validate: validateColor
   }
 ]
 
-// TODO: Create a function to create the SVG file
-function writeToFile (data) {
-  console.log(data)
-  /*const outputReadme = './output/README.md'
-  fs.writeFile(outputReadme, data, err => {
+// This function builds the string for the SVG based on user input and creates the file
+function writeToFile (answers) {
+  let chosenShape = ''
+  switch (answers.shape) {
+    case 'Triangle':
+      chosenShape = new Triangle(answers.bgColor)
+      break
+    case 'Square':
+      chosenShape = new Square(answers.bgColor)
+    case 'Circle':
+      chosenShape = new Circle(answers.bgColor)
+  }
+
+  // the logo will be a 300x200 pixel image
+  // build the svg string based on user input
+  let svg = `
+  <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg"><g>
+  ${chosenShape.render()}
+  <text x="50%" y="45%" text-anchor="middle" font-size="2rem" font-family="arial" fill="${
+    answers.textColor
+  }"> ${answers.text}</text></g></svg>`
+
+  // write the svg file to disk
+  fs.writeFile(FILENAME, svg, err => {
     if (err) console.log(err)
     else {
-      console.log('File written successfully\n')
-      console.log('The written has the following contents:')
-      console.log(fs.readFileSync(outputReadme, 'utf8'))
+      console.log('Generated logo.svg \n')
     }
-  })*/
+  })
 }
 
 // Create a function to initialize app
